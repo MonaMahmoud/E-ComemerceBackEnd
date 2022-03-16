a3afdd4d9cc6293af7d71b3e417c7f60b6bcfbc0
MonaMahmoud <monamohamedfahmy@gmail.com>
2022-03-16 22:03:49 +1100

    created-db-relations-inside-index.js

--- models/index.js
+++ models/index.js
@@ -6,15 +6,37 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.hasOne(Category, {
+    foreignKey: 'category_id',
+    onDelete: 'SET NULL',
+})
+
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Tag.hasMany(Product, {
+    throught: {
+        model: ProductTag,
+        unique: false,
+    }
+});
 
 // Tags belongToMany Products (through ProductTag)
 
+Product.hasMany(Tag, {
+    through: {
+        model: Tag,
+        unique: false,
+    }
+})
+
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
